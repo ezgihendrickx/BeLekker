d5e5b1d7c0ad55e6bd78703b04a64a064a7e2b42
ezgihendrickx <ezgihendrickx@gmail.com>
2020-10-28 16:00:10 +0300

    Add Navbar

---
+++ frontend/src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import React, { useState, useRef, useEffect } from "react";
+
+function Main() {
+  return (
+    <div className="Main">
+      <nav class="relative flex items-center justify-between h-12"></nav>
+      <main class="main bg-gradient-to-tr">
+        {/* <img class="block h-8 w-auto" src="/img/logo.png" alt="logo" /> */}
+      </main>
+    </div>
+  );
+}
+
+export default Main;
---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React, { useState, useRef, useEffect } from "react";
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <nav class="bg-red-800 relative flex items-center justify-between h-16">
+        <div class="logo">
+          LOGO
+          {/* <img class="block h-8 w-auto" src="/img/logo.png" alt="logo" /> */}
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
