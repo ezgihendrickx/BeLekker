c06451930ac2df45451e4069a13eea610bd86ed1
Mattias Bonte <wisecodr@gmail.com>
2020-10-30 14:08:35 +0100

    Update API Status Message & Routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.send('API operational ✅');
+  res.send({
+    Info: { Status: 'API operational ✅', DataBase: 'DB Connected ✅' },
+    routes: {
+      home: '/',
+      login: '/api/user/login',
+      register: '/api/user/register',
+      posts: '/api/posts',
+    },
+  });
 });
 
 module.exports = router;
