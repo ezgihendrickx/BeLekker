176ae9f768f0844787f341a76382b58cc1db56f7
ezgihendrickx <ezgihendrickx@gmail.com>
2020-10-30 13:04:19 +0300

    Set the Landingpage after Login

--- frontend/src/components/Main/Landingpage.js
+++ frontend/src/components/Main/Landingpage.js
@@ -11,61 +11,14 @@ function Landingpage() {
             type="search"
             placeholder="Search for a cooking experience..."
           />
-          <nav class="flex">
-            <a
-              class="no-underline text-white py-3 px-4 font-medium mr-3 bg-gray hover:bg-gray-darker"
-              href="#"
-            >
-              Cardamom
-            </a>
-            <a
-              class="no-underline text-white py-3 px-4 font-medium mx-3 bg-gray-darker hover:bg-gray"
-              href="#"
-            >
-              Cinnamon
-            </a>
-            <a
-              class="no-underline text-white py-3 px-4 font-medium mx-3 bg-gray hover:bg-gray-darker"
-              href="#"
-            >
-              Chamomille
-            </a>
-            <a
-              class="no-underline text-white py-3 px-4 font-medium mx-3 bg-gray-darker hover:bg-gray"
-              href="#"
-            >
-              Apple
-            </a>
-            <a
-              class="no-underline text-white py-3 px-4 font-medium mx-3 bg-gray hover:bg-gray-darker"
-              href="#"
-            >
-              Mint
-            </a>
-            <a
-              class="no-underline text-white py-3 px-4 font-medium mx-3 bg-gray-darker hover:bg-gray"
-              href="#"
-            >
-              Curry
-            </a>
-            <a
-              class="no-underline text-white py-3 px-4 font-medium mx-3 bg-gray hover:bg-gray-darker"
-              href="#"
-            >
-              Fragrance
-            </a>
-            <a
-              class="no-underline text-white py-3 px-4 font-medium ml-auto bg-gray-darker hover:bg-gray"
-              href="#"
-            ></a>
-          </nav>
         </div>
       </section>
       <div className="ml-6 mb-5 text-3xl italic leading-2 text-center text-red-800">
         <h2>What's on the Menu Today?</h2>
       </div>
-
-      <img src="../img/landing.png"></img>
+      <div class="items-center justify-center ml-40">
+        <img src="../img/landingpagetransparent.png" width="70%"></img>
+      </div>
     </div>
   );
 }
--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -6,7 +6,7 @@ import Landingpage from "./Landingpage";
 function Main() {
   //STATE
   const [authToken, setAuthToken] = useState("");
-  const [page, setPage] = useState("login");
+  const [page, setPage] = useState("login"); //when you open the browser it will be first page
 
   //FUNCTIONS
   const saveAuthToken = (token) => {
@@ -14,14 +14,20 @@ function Main() {
   };
 
   //RENDER JSX
+  //We have State and default state as Login
+  //This function below here is saying if my state login I will call the login component.
+  //In our Login page We have the "don't have an account yet button, if I click on it calls the function which changes the state page to the register page.
+  //And then function below calls the register component.
+
   return (
     <main className="place-content-center bg-cook-together h-screen bg-no-repeat bg-cover">
       {page === "login" ? (
         <Login saveAuthToken={saveAuthToken} setPage={setPage} />
-      ) : (
+      ) : page === "register" ? (
         <Register setPage={setPage} />
+      ) : (
+        <Landingpage setPage={setPage} />
       )}
-      <Landingpage />
     </main>
   );
 }
