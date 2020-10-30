1c35c24d376dcd5ce24f1a409478a869e51e6db3
ezgihendrickx <ezgihendrickx@gmail.com>
2020-10-30 08:30:18 +0300

    Change the title,icon,logo

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,11 +1,11 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from "react";
 
 function Navbar() {
   return (
-    <div className='Navbar'>
-      <nav className='relative flex items-center justify-between h-16 bg-red-800'>
-        <div className='logo'>
-          <img className='w-auto h-12 ml-5' src='/img/logo.png' />
+    <div className="Navbar">
+      <nav className="relative flex items-center justify-between h-16 bg-red-800">
+        <div className="logo">
+          <img className="w-auto h-12 ml-5" src="/img/ladle.png" />
         </div>
       </nav>
     </div>
