21968e45884bbe1e3625652d1c759a346d309d3a
Mattias Bonte <wisecodr@gmail.com>
2020-10-30 13:15:23 +0100

    Fix Login Connect with Landing Page

--- frontend/src/components/Main/Landingpage.js
+++ frontend/src/components/Main/Landingpage.js
@@ -1,23 +1,23 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from 'react';
 
 function Landingpage() {
   return (
     <div>
-      <section class="bg-gray-dark h-50">
-        <div class="container mx-auto">
-          <img class="m-5" src="../img/avatar.png" width="70px"></img>
+      <section className='bg-gray-dark h-50'>
+        <div className='container mx-auto'>
+          <img className='w-16 ml-5' src='../img/avatar.png'></img>
           <input
-            class="w-full h-16 px-3 rounded  focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg"
-            type="search"
-            placeholder="Search for a cooking experience..."
+            className='focus:outline-none focus:shadow-outline w-full h-16 px-3 px-8 text-xl rounded shadow-lg'
+            type='search'
+            placeholder='Search for a cooking experience...'
           />
         </div>
       </section>
-      <div className="ml-6 mb-5 text-3xl italic leading-2 text-center text-red-800">
+      <div className='leading-2 mb-5 ml-6 text-3xl italic text-center text-red-800'>
         <h2>What's on the Menu Today?</h2>
       </div>
-      <div class="items-center justify-center ml-40">
-        <img src="../img/landingpagetransparent.png" width="70%"></img>
+      <div className='items-center justify-center ml-40'>
+        <img src='../img/landingpagetransparent.png' width='70%'></img>
       </div>
     </div>
   );
--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import Login from "./Login";
-import Register from "./Register";
-import Landingpage from "./Landingpage";
+import React, { useState } from 'react';
+import Login from './Login';
+import Register from './Register';
+import Landingpage from './Landingpage';
 
 function Main() {
   //STATE
-  const [authToken, setAuthToken] = useState("");
-  const [page, setPage] = useState("login"); //when you open the browser it will be first page
+  const [authToken, setAuthToken] = useState('');
+  const [page, setPage] = useState('login'); //when you open the browser it will be first page
 
   //FUNCTIONS
   const saveAuthToken = (token) => {
@@ -20,14 +20,16 @@ function Main() {
   //And then function below calls the register component.
 
   return (
-    <main className="place-content-center bg-cook-together h-screen bg-no-repeat bg-cover">
-      {page === "login" ? (
-        <Login saveAuthToken={saveAuthToken} setPage={setPage} />
-      ) : page === "register" ? (
-        <Register setPage={setPage} />
-      ) : (
-        <Landingpage setPage={setPage} />
-      )}
+    <main className='place-content-center bg-cook-together h-screen bg-no-repeat bg-cover'>
+      <main className='place-content-center bg-cook-together h-screen bg-no-repeat bg-cover'>
+        {page === 'login' ? (
+          <Login saveAuthToken={saveAuthToken} setPage={setPage} />
+        ) : (
+          ''
+        )}
+        {page === 'register' ? <Register setPage={setPage} /> : ''}
+        {page === 'landing' ? <Landingpage setPage={setPage} /> : ''}
+      </main>
     </main>
   );
 }
